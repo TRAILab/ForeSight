19c4c7b568749cbc7cff79c01f566a49d06b54ba
sandropapais <papais.sandro@gmail.com>
2026-02-25 20:53:25 -0500

    remove cache failed fix

--- scripts/dgx_run.sh
+++ scripts/dgx_run.sh
@@ -20,7 +20,6 @@ CMD=${@:-bash}
 echo "Running: $CMD"
 singularity exec --nv -e --pwd /workspace/ForeSight/ \
     --env="WANDB_API_KEY=$WANDB_API_KEY" \
-    --env="PYTHONPYCACHEPREFIX=/tmp/pycache" \
     --bind=$CODE_DIR:/workspace/ForeSight/ \
     --bind=$DATA_DIR:/workspace/ForeSight/data/nuscenes \
     docker/foresight.sif $CMD
