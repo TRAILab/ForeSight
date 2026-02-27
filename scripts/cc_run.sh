5994e267711bb58b3db121aa82d997fa7203cbf5
sandropapais <papais.sandro@gmail.com>
2026-02-27 02:17:23 -0500

    debug setup

--- scripts/cc_run.sh
+++ scripts/cc_run.sh
@@ -6,7 +6,7 @@
 #SBATCH --time=2:59:00 # 3 hours or 12 hours max recommended
 #SBATCH --output=/home/spapais/ForeSight/logs/%x-%j.log
 #SBATCH --cpus-per-task=12
-#SBATCH --gres=gpu:a100:4 # gpu:h100:4 (trillium) or gpu:a100:4 (narval)
+#SBATCH --gres=gpu:a100:1 # gpu:h100:4 (trillium) or gpu:a100:4 (narval)
 #SBATCH --mail-user="sandro.papais@robotics.utias.utoronto.ca"
 #SBATCH --mail-type=END,FAIL
 
