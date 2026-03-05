d843c81df5878b026a415dbe8948463c7b65c8fb
sandropapais <papais.sandro@gmail.com>
2026-03-05 06:36:03 -0500

    visloss error

--- projects/mmdet3d_plugin/models/detection3d/detection3d_head.py
+++ projects/mmdet3d_plugin/models/detection3d/detection3d_head.py
@@ -688,7 +688,7 @@ class Sparse4DHead(BaseModule):
                         )
                 matched = mask_valid.reshape(-1)
                 vis_loss = self.loss_visibility(
-                    vis.squeeze(-1).flatten(end_dim=1)[matched],
+                    vis.squeeze(-1).flatten(end_dim=1)[matched].unsqueeze(-1),
                     vis_target.flatten(end_dim=1)[matched].long(),
                     avg_factor=num_pos,
                 )
