7f0cb008950bdbbb6a2d1eace6a67ec4937ee7b2
sandropapais <papais.sandro@gmail.com>
2026-04-25 05:31:14 -0400

    sd_combined: fix map refine return to 5-tuple
    Sparse4DHead.forward now unpacks (output, cls, qt, vis, rel) from refine.
    SparsePoint3DRefinementModule (used by map head) only returned 4 values,
    causing 'not enough values to unpack' on map_head. Pad its return to 5
    with None for relevance; map head doesn't predict relevance.

--- projects/mmdet3d_plugin/models/map/map_blocks.py
+++ projects/mmdet3d_plugin/models/map/map_blocks.py
@@ -87,7 +87,7 @@ class SparsePoint3DRefinementModule(BaseModule):
         else:
             cls = None
         qt = None
-        return output, cls, qt, None
+        return output, cls, qt, None, None
 
 
 @PLUGIN_LAYERS.register_module()
